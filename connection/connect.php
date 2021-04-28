63e4f92807d1a49145de9a3f8edc65916f6dcbcd
KrisnaWahyu <krisnawyu@webmail.umm.ac.id>
2021-04-29 05:34:54 +0700

    Config DB

--- connection/connect.php
+++ connection/connect.php
@@ -1,9 +1,9 @@
 <?php
 
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "uts_resto";
+$username = "bc910733047971";
+$password = "46552ef2";
+$dbname = "	heroku_ec1009a765ac2cf";
 
 $db = mysqli_connect($servername, $username, $password, $dbname);
 
